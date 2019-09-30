c4a31c109aa6ece63b54f257be25f8857b5d7545
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2019-09-30 16:19:50 +0900

    iosアプリ

---
+++ iosTask/iosTask/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iosTask
+//
+//  Created by Papillon on 2019/09/30.
+//  Copyright © 2019 Papillon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
